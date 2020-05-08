7c1ee7c453b046216cc07e7b45fe96bb8141d108
SadowyKinga <64136124+SadowyKinga@users.noreply.github.com>
2020-05-08 22:47:55 +0200

    Dodanie klasy Monety oraz klasy Produkty.

---
+++ Automat z napojami.py
@@ -0,0 +1,42 @@
+import random as r
+
+#--------------------- KLASA MONET ------------------------------------------
+class Monety(object):
+    dostepnosc_monety = []
+    nominaly = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0]
+    for i in range(30):
+        x = r.choice(nominaly)
+        dostepnosc_monety.append(x)
+    dostepnosc_monety.sort(reverse = True)
+    print("\nMonety dostępne w automacie to:\n")
+    print(dostepnosc_monety)
+
+    def __init__(self): #self - przez to kazdy obiekt jest widziany sam przez siebie, nazwa self winna byc pierwszym argumentem wszystkich deklarowanych metod
+        dostepnosc_monety = self.dostepnosc_monety
+
+    def dodaj_monety(self, moneta):
+        self.dostepnosc_monety.append(moneta)
+        print(self.dostepnosc_monety)
+
+    def bierz_monety(self, nominal):
+        self.dostepnosc_monety.remove(nominal)
+
+#--------------------- KLASA PRODUKTOW ------------------------------------------
+class Produkty(object):
+    asortyment = [[30, 4.51, 5], [31, 1.10, 5], [32, 4.35, 5], [33, 5.0, 5], [34, 0.55, 5],
+                  [35, 5.20, 5], [36, 8.0, 5], [37, 6.20, 5], [38, 4.25, 5], [39, 5.10, 5],
+                  [40, 2.60, 5], [41, 4.15, 5], [42, 7.50, 5], [43, 5.25, 5], [44, 1.60, 1],
+                  [45, 2.0, 5], [46, 5.52, 5], [47, 8.0, 5], [48, 0.75, 5], [49, 1.90, 5], [50, 1.05, 5]]
+
+    def podaj_cene(self, numer): 
+        for x in range(len(self.asortyment)):
+            if self.asortyment[x][0] == numer:
+                return self.asortyment[x][1]
+
+    def podaj_ilosc(self, numer):
+        for x in range(len(self.asortyment)):#len - pokazuje ilosc znakow w stringu
+            if self.asortyment[x][0] == numer:
+                return self.asortyment[x][2]
+    print("\nAsortyment w automacie to:\n")
+    print(asortyment)
+    
