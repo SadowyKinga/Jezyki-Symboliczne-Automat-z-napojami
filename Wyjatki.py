a42d40f41c3e25545cc7eb6b37092bc7e5a2a417
SadowyKinga <64136124+SadowyKinga@users.noreply.github.com>
2020-05-24 15:44:52 +0200

    zastąpienie słowa pass docstringami dla każdego z poszczególnych wyjątków

--- Wyjatki.py
+++ Wyjatki.py
@@ -1,13 +1,15 @@
-"""Klasy wyjatkow slużą do opisów błedów, nie kończa programu
-    KLASY:
-    *BladNumeruLubBrakProduktu - informuje klienta, iż najprawdopodobniej podał zly numer produktu, lub produkt został wyczerpany, ,
-    *BlednyNumer - informuje klienta, iż podał niepoprawny numer produktu,
-    *ProduktWyprzedany - informuje klienta, iz dany produkt został wyprzedany."""
-
 #----------------------- WYJATKI ---------------------------------------------
 class BladNumeruLubBrakProduktu(Exception):
-    pass
+    """Klasy wyjatkow slużą do opisów błedów, nie kończa programu
+    KLASY:
+    *BladNumeruLubBrakProduktu - informuje klienta, iż najprawdopodobniej podał zly numer produktu, lub produkt został wyczerpany """
+    
 class BlednyNumer(Exception):
-    pass
+    """Klasy wyjatkow slużą do opisów błedów, nie kończa programu
+    KLASY:
+    *BlednyNumer - informuje klienta, iż podał niepoprawny numer produktu """
+    
 class ProduktWyprzedany(Exception):
-    pass
+    """Klasy wyjatkow slużą do opisów błedów, nie kończa programu
+    KLASY:
+    *ProduktWyprzedany - informuje klienta, iz dany produkt został wyprzedany."""
